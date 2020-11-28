ef54dbf6d099dd2cc48cd6d272b9badc88e1bf6a
Geovane Silva <geovaneb.gb@hotmail.com>
2020-11-28 15:09:57 -0300

    Alterando metodos para assincronos

--- SalesWeb/Services/DepartmentService.cs
+++ SalesWeb/Services/DepartmentService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore;
+
 using SalesWeb.Data;
 using SalesWeb.Models;
 
@@ -17,9 +19,9 @@ namespace SalesWeb.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
