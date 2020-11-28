eeea72e2faac488ebe73d10f236b2b1d523dec8c
Geovane Silva <geovaneb.gb@hotmail.com>
2020-11-28 12:49:51 -0300

    Implementado os detalhes

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,4 +1,6 @@
-﻿using SalesWeb.Data;
+﻿using Microsoft.EntityFrameworkCore;
+
+using SalesWeb.Data;
 using SalesWeb.Models;
 
 using System.Collections.Generic;
@@ -26,7 +28,7 @@ namespace SalesWeb.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
