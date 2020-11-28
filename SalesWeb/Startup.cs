4fc8daca2b5a05d91c4ff44c3f9b3191bb23c8e6
Geovane Silva <geovaneb.gb@hotmail.com>
2020-11-28 14:11:57 -0300

    Definida a localidade padrão da aplicação

--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWeb.Data;
 using SalesWeb.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using System.Collections.Generic;
+
 namespace SalesWeb
 {
     public class Startup
@@ -31,8 +35,8 @@ namespace SalesWeb
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<SalesWebContext>(options => 
-                options.UseMySql(Configuration.GetConnectionString("SalesWebContext"), builder => 
+            services.AddDbContext<SalesWebContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("SalesWebContext"), builder =>
                     builder.MigrationsAssembly("SalesWeb")));
 
             services.AddScoped<SeedingService>();
@@ -43,6 +47,16 @@ namespace SalesWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
